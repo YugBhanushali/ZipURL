3c6b98c85684c2951c81f97ca029127bbccc6499
YugBhanushali <yughbhanushali241103@gmail.com>
2023-08-11 23:12:54 +0530

    added meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { Roboto_Mono,JetBrains_Mono,Roboto_Condensed,Roboto_Flex } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
 import logo from './icon.png';
+import { URL_OF_WEBSITE } from '@/utils/constants';
 
 
 
@@ -13,28 +14,8 @@ const robotoMono = JetBrains_Mono({
 })
 
 export const metadata = {
-
   title: 'ZipURL',
   description: 'Get your zipped url with the name you want and track the analytics of your zipped url wiht few clicks. ZipURL is a free url shortener service which allows you to create short links for your long links and share them easily. Now shorten your links and make them more manageable.',
-  metadata: [
-
-    {name: 'viewport', content: 'width=device-width, initial-scale=1.0'},
-
-    { name:'keywords', content:'url shortener, url shortener free, url shortener custom, url shortener custom name, url shortener custom link, url shortener custom domain, url shortener, short url, zip url, small url, manage url, links, share links '},
-
-    { name: 'og:title', content: 'ZipURL' },
-    { name: 'og:description', content: 'Get your zipped url with the name you want and track the analytics of your zipped url wiht few clicks. ZipURL is a free url shortener service which allows you to create short links for your long links and share them easily. Now shorten your links and make them more manageable.' },
-    { name: 'og:image', content: '/icon.png' },
-    { name: 'og:url', content: 'https://zipurl.tech/' },
-    { name: 'og:type', content: 'website' },
-
-    { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:site', content: '@zipurl' },
-    { name: 'twitter:creator', content: '@zipurl' },
-    { name: 'twitter:title', content: 'ZipURL' },
-    { name: 'twitter:description', content: 'Get your zipped url with the name you want and track the analytics of your zipped url wiht few clicks. ZipURL is a free url shortener service which allows you to create short links for your long links and share them easily. Now shorten your links and make them more manageable.' },
-    { name: 'twitter:image', content: '/icon.png' },
-  ],
 }
 
 export default function RootLayout({
@@ -45,6 +26,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content="url shortener, url shortener free, url shortener custom, url shortener custom name, url shortener custom link, url shortener custom domain, url shortener, short url, zip url, small url, manage url, links, share links" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={`${URL_OF_WEBSITE}assets/zipurl-demo.png`} />
+        <meta property="og:url" content="https://zipurl.vercel.app/" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@zipurl" />
+        <meta name="twitter:creator" content="@zipurl" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={`${URL_OF_WEBSITE}assets/zipurl-demo.png`} />
+
         <link rel="icon" href="/icon.png" type="image/png" />
         <link rel="shortcut icon" href="./icon.png" type="image/png" />
       </head>
